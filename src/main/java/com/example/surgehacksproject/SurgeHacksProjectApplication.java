e37af446b4ca19e3a9c6fe759cda4323f086672e
cammc <cam.mclagan@gmail.com>
2024-05-18 14:35:19 -0700

    First Push

---
+++ src/main/java/com/example/surgehacksproject/SurgeHacksProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.surgehacksproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurgeHacksProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurgeHacksProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/surgehacksproject/SurgeHacksProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.surgehacksproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurgeHacksProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
